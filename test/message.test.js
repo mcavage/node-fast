f52bb10aa1f4a996b3101e2d1b9607b6a040f53b
Mark Cavage <mcavage@gmail.com>
2013-08-27 11:32:13 -0700

    run RPC requests in domain contexts

--- test/message.test.js
+++ test/message.test.js
@@ -3,7 +3,7 @@
 var fast = require('../lib');
 
 if (require.cache[__dirname + '/helper.js'])
-        delete require.cache[__dirname + '/helper.js'];
+    delete require.cache[__dirname + '/helper.js'];
 var test = require('./helper.js').test;
 
 
@@ -11,59 +11,59 @@ var test = require('./helper.js').test;
 ///--- Tests
 
 test('serialize ok', function (t) {
-        var encoder = new fast.MessageEncoder();
+    var encoder = new fast.MessageEncoder();
 
-        encoder.on('readable', function onReadable() {
-                var buf = encoder.read();
-                t.ok(buf);
-                t.equal(buf[0], 0x01); // v
-                t.equal(buf[1], 0x01); // t
-                t.equal(buf[2], 0x01); //s
-                t.equal(buf.readUInt32BE(3), 123); // id
-                t.ok(buf.readInt32BE(7)); // crc
-                t.equal(buf.readUInt32BE(11), 17); // len
-                t.deepEqual(JSON.parse(buf.slice(15, 32).toString()), {
-                        hello: 'world'
-                });
-                t.done();
+    encoder.on('readable', function onReadable() {
+        var buf = encoder.read();
+        t.ok(buf);
+        t.equal(buf[0], 0x01); // v
+        t.equal(buf[1], 0x01); // t
+        t.equal(buf[2], 0x01); //s
+        t.equal(buf.readUInt32BE(3), 123); // id
+        t.ok(buf.readInt32BE(7)); // crc
+        t.equal(buf.readUInt32BE(11), 17); // len
+        t.deepEqual(JSON.parse(buf.slice(15, 32).toString()), {
+            hello: 'world'
         });
-        encoder.send({
-                msgid: 123,
-                data: {
-                        hello: 'world'
-                },
-                status: 0x01,
-                type: 0x01,
-                version: 0x01
-        });
+        t.done();
+    });
+    encoder.send({
+        msgid: 123,
+        data: {
+            hello: 'world'
+        },
+        status: 0x01,
+        type: 0x01,
+        version: 0x01
+    });
 });
 
 
 test('deserialize ok', function (t) {
-        var encoder = new fast.MessageEncoder();
-        var decoder = new fast.MessageDecoder();
+    var encoder = new fast.MessageEncoder();
+    var decoder = new fast.MessageDecoder();
 
 
-        var msg1 = {
-                msgid: 123,
-                data: {
-                        hello: 'world'
-                },
-                status: 0x01,
-                type: 0x01,
-                version: 0x01
-        };
+    var msg1 = {
+        msgid: 123,
+        data: {
+            hello: 'world'
+        },
+        status: 0x01,
+        type: 0x01,
+        version: 0x01
+    };
 
-        decoder.on('message', function (msg2) {
-                t.ok(msg2);
-                t.equal(msg2.msgid, msg1.msgid);
-                t.equal(msg2.status, msg1.status);
-                t.equal(msg2.type, msg1.type);
-                t.equal(msg2.version, msg1.version);
-                t.deepEqual(msg2.data, msg1.data);
-                t.done();
-        });
+    decoder.on('message', function (msg2) {
+        t.ok(msg2);
+        t.equal(msg2.msgid, msg1.msgid);
+        t.equal(msg2.status, msg1.status);
+        t.equal(msg2.type, msg1.type);
+        t.equal(msg2.version, msg1.version);
+        t.deepEqual(msg2.data, msg1.data);
+        t.done();
+    });
 
-        encoder.pipe(decoder);
-        encoder.send(msg1);
+    encoder.pipe(decoder);
+    encoder.send(msg1);
 });
--- test/rpc.test.js
+++ test/rpc.test.js
@@ -3,7 +3,7 @@
 var fast = require('../lib');
 
 if (require.cache[__dirname + '/helper.js'])
-        delete require.cache[__dirname + '/helper.js'];
+    delete require.cache[__dirname + '/helper.js'];
 var test = require('./helper.js').test;
 
 
@@ -20,91 +20,136 @@ var server;
 ///--- Tests
 
 test('createServer', function (t) {
-        server = fast.createServer();
-        t.ok(server);
-        t.done();
+    server = fast.createServer();
+    t.ok(server);
+    t.done();
 });
 
 
 test('listen', function (t) {
-        server.listen(PORT, function () {
-                t.done();
-        });
+    server.listen(PORT, function () {
+        t.done();
+    });
 });
 
 
 test('createClient', function (t) {
-        client = fast.createClient({
-                host: 'localhost',
-                port: PORT
-        });
-        client.on('connect', function () {
-                t.done();
-        });
+    client = fast.createClient({
+        host: 'localhost',
+        port: PORT
+    });
+    client.on('connect', function () {
+        t.done();
+    });
 });
 
 
 test('echo RPC handler', function (t) {
-        server.rpc('echo', function (message, res) {
-                res.end(message);
-        });
-        var req = client.rpc('echo', 'hello world');
-        t.ok(req);
-        req.on('message', function (msg) {
-                t.equal(msg, 'hello world');
-        });
-        req.on('end', function () {
-                t.done();
-        });
+    server.rpc('echo', function (message, res) {
+        res.end(message);
+    });
+    var req = client.rpc('echo', 'hello world');
+    t.ok(req);
+    req.on('message', function (msg) {
+        t.equal(msg, 'hello world');
+    });
+    req.on('end', function () {
+        t.done();
+    });
 });
 
 
 test('error RPC handler', function (t) {
-        server.rpc('err', function (res) {
-                var e = new Error('suck it, mr. client');
-                e.context = {
-                        foo: 'bar'
-                };
-                res.write(e);
-        });
-        var req = client.rpc('err');
-        t.ok(req);
-        req.on('error', function (err) {
-                t.ok(err);
-                t.equal(err.message, 'suck it, mr. client');
-                t.ok(err.context);
-                if (err.context)
-                        t.equal(err.context.foo, 'bar');
-                t.done();
-        });
+    server.rpc('err', function (res) {
+        var e = new Error('suck it, mr. client');
+        e.context = {
+            foo: 'bar'
+        };
+        res.write(e);
+    });
+    var req = client.rpc('err');
+    t.ok(req);
+    req.on('error', function (err) {
+        t.ok(err);
+        t.equal(err.message, 'suck it, mr. client');
+        t.ok(err.context);
+        if (err.context)
+            t.equal(err.context.foo, 'bar');
+        t.done();
+    });
 });
 
 
 test('streaming RPC handler', function (t) {
-        server.rpc('stream', function (res) {
-                for (var i = 1; i <= 10; i++)
-                        res.write({i: i});
-                res.end();
+    server.rpc('stream', function (res) {
+        for (var i = 1; i <= 10; i++)
+            res.write({i: i});
+        res.end();
+    });
+    var req = client.rpc('stream');
+    var seen = 0;
+    t.ok(req);
+    req.on('message', function (obj) {
+        t.ok(obj);
+        t.ok(obj.i);
+        seen++;
+    });
+    req.on('end', function () {
+        t.equal(seen, 10);
+        t.done();
+    });
+});
+
+
+test('RPC handler with thrown error #1', function (t) {
+    server.rpc('echo', function (message, res) {
+        process.nextTick(function () {
+            throw new Error('boom');
         });
-        var req = client.rpc('stream');
-        var seen = 0;
-        t.ok(req);
-        req.on('message', function (obj) {
-                t.ok(obj);
-                t.ok(obj.i);
-                seen++;
+    });
+
+    server.once('error', function (err, msg, res) {
+        t.ok(err);
+        t.ok(msg);
+        t.ok(res);
+        res.end(err);
+    });
+    var req = client.rpc('echo', 'foo');
+    t.ok(req);
+    req.on('error', function (err) {
+        t.ok(err);
+        t.done();
+    });
+});
+
+
+
+test('RPC handler with thrown error #2', function (t) {
+    server.rpc('echo', function (message, res) {
+        process.nextTick(function () {
+            throw new Error('boom');
         });
-        req.on('end', function () {
-                t.equal(seen, 10);
-                t.done();
-        });
+    });
+
+    server.once('uncaughtException', function (err, msg, res) {
+        t.ok(err);
+        t.ok(msg);
+        t.ok(res);
+        res.end(err);
+    });
+    var req = client.rpc('echo', 'foo');
+    t.ok(req);
+    req.on('error', function (err) {
+        t.ok(err);
+        t.done();
+    });
 });
 
 
 test('teardown', function (t) {
-        server.on('close', function () {
-                t.done();
-        });
-        client.close();
-        server.close();
+    server.on('close', function () {
+        t.done();
+    });
+    client.close();
+    server.close();
 });
