3e4ad857af28a277f1218544a26f714b5102f341
Julien Gilli <julien.gilli@joyent.com>
2016-01-19 12:25:25 -0800

    MORAY-324: client sometimes hang after a call to close() Reviewed by: Patrick Mooney <patrick.mooney@joyent.com>

--- test/client.test.js
+++ test/client.test.js
@@ -177,3 +177,82 @@ test('RPC error when not connected', function (t) {
         });
     });
 });
+
+// Regression test for https://smartos.org/bugview/MORAY-324.
+test('socket properly hangs up via close', function (t) {
+        var client1EmittedClose = false;
+
+        server = fast.createServer();
+        server.listen(PORT, function () {
+            // Create a first client that we'll close right away.
+            // The goal is to reproduce the issue described by MORAY-324 where
+            // a client that would be immediately closed before establishing
+            // a connection would _not_ be closed, and would still connect
+            // to the server.
+            client = fast.createClient({
+                host: HOST,
+                port: PORT
+            });
+
+            client.close();
+
+            client.on('connect', function onClosedClientConnect() {
+                t.ok(false, 'closed client should not emit connect event');
+            });
+
+            client.on('close', function onClient1Close() {
+                client1EmittedClose = true;
+            });
+
+            // Create a second client, only for the purpose of making
+            // sure that the first one has the time to connect if the bug
+            // described in MORAY-324 is still present.
+            var client2 = fast.createClient({
+                host: HOST,
+                port: PORT
+            });
+
+            // Close the second client as soon as it connects so that it
+            // doesn't hold the libuv event loop open and allows the server
+            // to close (and thus the test to end) if the first client
+            // manages to close its connection.
+            client2.on('connect', function onClient2Connected() {
+                client2.close();
+            });
+
+            client2.on('close', function onClient2Closed() {
+                // When the second client closes, if the bug described by
+                // MORAY-324 is still present, the first client will have
+                // established a connection, and the server won't be able to
+                // close since the first client will never close.
+                // If MORAY-324 is fixed, the first client will have closed its
+                // connection before it's established, and thus as soon as the
+                // second client closes its connection, the server can close
+                // and the test can end.
+                server.close();
+
+                t.equal(client1EmittedClose, true, 'first client should ' +
+                    'have emitted close');
+
+                // Use a timeout to check for the number of current connections
+                // on the server, as when client2 closed its connection, the
+                // other end of the connection may not have closed yet.
+                // Delay of 1000ms is arbitrary, but should be enough to let
+                // the server side of the connection to close, and the
+                // net.Server's .connections property to be updated.
+                setTimeout(function waitForClientsClose() {
+                    server.srv.getConnections(function (err, nbConnections) {
+                        t.ifError(err, 'getConnections should not result in ' +
+                            'an error');
+                        t.equal(nbConnections, 0,
+                            'after second client closed, server should have ' +
+                            'no remaining client connected');
+                    });
+                }, 1000);
+            });
+        });
+
+        server.on('close', function onServerClosed() {
+            t.end();
+        });
+    });
